b3dd967d7684d6f2c63542c78b35fb14ccda2b5c
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-16 18:01:59 +0530

    conditionals

---
+++ javaconcepts/conditionals.java
@@ -0,0 +1,41 @@
+package javaconcepts;
+
+public class conditionals {
+    public static void main(String[] args) {
+        // Syntax of if-else statement:
+        /* if (condition-to-be-checked) {
+	statements-if-condition-true;
+}
+else {
+	statements-if-condition-false;
+} */
+
+        // Example
+
+        int a = 29;
+        if (a>18) {
+            System.out.println("You can drive");
+        }
+        else{
+            System.out.println("You are underage!");
+        }
+
+
+        // if-else ladder:
+        /* if (condition1) {
+
+            //Statements;
+
+else if {
+
+            // Statements;
+
+}
+
+else {
+
+            //Statements
+
+} */
+    }
+}
