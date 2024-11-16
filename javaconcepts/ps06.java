f90343c6aba38510e8634ce72e36d2e46f1754d2
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-17 00:41:45 +0530

    ps_06 completed.

--- javaconcepts/ps06.java
+++ javaconcepts/ps06.java
@@ -97,6 +97,20 @@ public class ps06 {
 //        System.out.println(Integer.MIN_VALUE);
 //        System.out.println(Integer.MAX_VALUE);
 
-        
+        // Practice Problem 8
+        boolean isSorted = true;
+        int [] arr = {1, 12, 3, 4, 5, 34, 67};
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i] > arr[i+1]){
+                isSorted = false;
+                break;
+            }
+        }
+        if(isSorted){
+            System.out.println("The Array is sorted");
+        }
+        else{
+            System.out.println("The Array is not sorted");
+        }
     }
 }
