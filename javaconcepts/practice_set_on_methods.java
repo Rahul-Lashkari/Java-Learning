10438b5c7ce50d60ced64ee25ea4fe40dd1dda53
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-18 19:49:23 +0530

    prob 2

--- javaconcepts/practice_set_on_methods.java
+++ javaconcepts/practice_set_on_methods.java
@@ -7,8 +7,29 @@ public class practice_set_on_methods {
         }
     }
 
+    static void pattern1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
         // Problem 1
-        multiplication(7);
+//        multiplication(7);
+
+        // Problem 2
+        pattern1(9);
+
+
+
+
     }
 }
