d016363511c16e159586c0f3043e8af7bd9e23a7
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-19 17:55:52 +0530

    create custom class

---
+++ javaconcepts/custom_class.java
@@ -0,0 +1,40 @@
+package javaconcepts;
+
+class Employee{
+    int id;
+    int salary;
+    String name;
+    public void printDetails(){
+        System.out.println("My id is " + id);
+        System.out.println("and my name is " + name);
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+}
+
+public class custom_class {
+    public static void main(String[] args) {
+        System.out.println("This is our custom class");
+        Employee Sasha = new Employee();   // Instantiating a new Employee Object
+        Employee Nicole = new Employee();  // Instantiating a new Employee Object
+
+        // Setting Attributes for Sasha
+        Sasha.id = 11;
+        Sasha.salary = 20;
+        Sasha.name = "Sasha";
+
+            // Setting Attributes for Nicole
+        Nicole.id = 12;
+        Nicole.salary = 18;
+        Nicole.name = "Nicole";
+
+        // Printing the Attributes~~
+        Sasha.printDetails();
+        Nicole.printDetails();
+        int salary = Sasha.getSalary();
+        System.out.println(salary);
+
+    }
+}
