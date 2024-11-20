663b36bc56bed593a40b920d8dbdb50b8b5a52ef
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-20 10:26:46 +0530

    ps09_prob1

---
+++ javaconcepts/ps09.java
@@ -0,0 +1,40 @@
+package javaconcepts;
+
+class Cylinder{
+    private int radius;
+    private int height;
+
+    public Cylinder(int radius, int height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
+
+public class ps09 {
+    public static void main(String[] args) {
+        // Problem 1
+        Cylinder myCylinder = new Cylinder(9, 12);
+        //myCylinder.setHeight(12);
+        System.out.println(myCylinder.getHeight());
+        //myCylinder.setRadius(9);
+        System.out.println(myCylinder.getRadius());
+    }
+}
+
+
