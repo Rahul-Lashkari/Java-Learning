33374e753ae199c9b71240b37f9bafffefa0103a
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-20 23:00:45 +0530

    prob 2

--- javaconcepts/ps09.java
+++ javaconcepts/ps09.java
@@ -24,16 +24,29 @@ class Cylinder{
     public void setHeight(int height) {
         this.height = height;
     }
+
+    public double surfaceArea(){
+        return 2* Math.PI* radius * radius + 2*Math.PI*radius*height;
+    }
+
+    public double volume(){
+        return Math.PI * radius * radius * height;
+    }
 }
 
 public class ps09 {
     public static void main(String[] args) {
-        // Problem 1
+         // Problem 1
         Cylinder myCylinder = new Cylinder(9, 12);
         //myCylinder.setHeight(12);
         System.out.println(myCylinder.getHeight());
         //myCylinder.setRadius(9);
         System.out.println(myCylinder.getRadius());
+
+
+        // Problem 2
+        System.out.println(myCylinder.surfaceArea());
+        System.out.println(myCylinder.volume());
     }
 }
 
