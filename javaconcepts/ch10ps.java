452b41cd3b76831368e541a44409d9e43a1db4c5
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-21 23:15:54 +0530

    ch10ps update

--- javaconcepts/ch10ps.java
+++ javaconcepts/ch10ps.java
@@ -15,9 +15,22 @@ class Circle{
     }
 }
 
+class Cylinder1 extends Circle{
+    public int height;
+    Cylinder1(int r, int h){
+        super(r);
+        System.out.println("I am cylinder1 parameterized constructor");
+        this.height = h;
+    }
+    public double volume(){
+        return Math.PI*this.radius*this.radius*this.height;
+    }
+}
+
 public class ch10ps {
     public static void main(String[] args) {
         // Problem 1
-        Circle objC = new Circle(12);
+        // Circle objC = new Circle(12);
+        Cylinder1 obj = new Cylinder1(12, 4);
     }
 }
