b3f4c6d54fbc4c0eb10ef793356ead87a89721ea
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-23 19:46:39 +0530

    ps q3

--- javaconcepts/ch11ps.java
+++ javaconcepts/ch11ps.java
@@ -16,10 +16,43 @@ class FountainPen extends Pen{
         System.out.println("Changing the nib");
     }
 }
+class Monkey{
+    void jump(){
+        System.out.println("Jumping...");
+    }
+    void bite(){
+        System.out.println("Biting...");
+    }
+}
+
+interface BasicAnimal{
+    void eat();
+    void sleep();
+}
+
+class Human extends Monkey implements BasicAnimal{
+    void speak(){
+        System.out.println("Hello sir!");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Sleeping");
+    }
+}
 public class ch11ps {
     public static void main(String[] args) {
         // Q1 + Q2
         FountainPen pen = new FountainPen();
         pen.changeNib();
+
+        // Q3
+        Human harry = new Human();
+        harry.sleep();
     }
 }
