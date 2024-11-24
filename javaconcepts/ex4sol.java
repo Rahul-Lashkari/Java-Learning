f19ab9982a840c88b054eba4ae76f8d5d67686c7
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-24 22:36:38 +0530

    ex4_problem 2

--- javaconcepts/ex4sol.java
+++ javaconcepts/ex4sol.java
@@ -2,7 +2,7 @@ package javaconcepts;
 
 public class ex4sol {
     public static void main(String[] args) {
-
+/*
         // Problem 1 - Reverse Words
         String sentence = "Hello World";
         String[] words = sentence.split(" ");
@@ -11,5 +11,20 @@ public class ex4sol {
             reversed.append(words[i]).append(" ");
         }
         System.out.println("Reversed Sentence: " + reversed.toString().trim());
+*/
+        // Problem 2
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+        System.out.println("Second Largest: " + secondLargest);
     }
 }
