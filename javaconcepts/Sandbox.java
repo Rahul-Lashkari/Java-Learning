140beffc203094110496b0e24916d2b955c684bc
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-25 22:53:00 +0530

    chapter 6 revision

--- javaconcepts/Sandbox.java
+++ javaconcepts/Sandbox.java
@@ -100,6 +100,38 @@ public class Sandbox {
 --
  */
 
+        // Chapter 6-10 revision :-
+/*
+        int n = 4;
+        for (int i=n; i>0;  i--){
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+-----
+        int sum = 0;
+        int n=4;
+        for(int i=0;i<n; i++){
+            sum = sum + (2*i);
+        }
+        System.out.print("Sum of even numbers is: ");
+        System.out.println(sum);
+-----
+        int n=2;
+        int x=10;
+        for(int i=1;i<=x;i++){
+            System.out.printf("%d X %d = %d\n", n, i, n*i);
+        }
+-----
+        int n=2;
+        int x=0;
+        for(int i=10;i>=x;i--){
+            System.out.printf("%d X %d = %d\n", n, i, n*i);
+        }
+----
+*/
+        
 
     }
 }
