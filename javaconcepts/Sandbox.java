0634a3f624dacea7f1a9cea6cb79034372aae68f
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-11-25 13:00:56 +0530

    Chapters 1-5 revision

---
+++ javaconcepts/Sandbox.java
@@ -0,0 +1,105 @@
+package javaconcepts;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Sandbox {
+    public static void main(String[] args) {
+
+        // Revisions & Repeated Practices are done in Sandbox! ~~~~
+
+        // Trying to do a fast Recap on weekend just to brush up the concepts😞..
+        // Dated - 24/11/24/Sunday
+
+/*
+        // Chapter 1-5 revision :-
+
+        String name = "Rahul Lashkari";
+        name = name.replace(" ", "_");
+        System.out.println(name);
+--
+        String letter = "Dear <|name|>, Thanks a lot!";
+        letter = letter.replace("<|name|>", "Sachin");
+        System.out.println(letter);
+--
+        String mystring = "This string contains double and  triple spaces";
+        System.out.println(mystring.indexOf("  "));
+        System.out.println(mystring.indexOf("   "));
+--
+        String myLetter = "Dear Harry,\n\tThis Java Course is Nice.\nThanks!";
+        System.out.println(myLetter);
+--
+        byte m1, m2, m3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your phy marks");
+        m1 = sc.nextByte();
+        System.out.println("Enter your eng marks");
+        m2 = sc.nextByte();
+        System.out.println("Enter your sci marks");
+        m3 = sc.nextByte();
+        float avg = (m1+m2+m3)/3.0f;
+        if(avg>=40 && m1>=33 && m2>=33 && m3>=33){
+            System.out.println("Congratulations");
+        }
+        else{
+            System.out.println("Sorry");
+        }
+--
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ur income in lakhs per annum");
+        float tax = 0;
+        float income = sc.nextFloat();
+        if (income <= 2.5) {
+            tax = tax + 0;
+        }
+        else if(income>2.5f && income<=5f) {
+            tax = tax + 0.05f * (income - 2.5f);
+        }
+        else if(income>5f && income <= 10.0f){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.2f * (income - 5f);
+        }
+        else if(income>10.0f){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.2f * (10.0f - 5f);
+            tax = tax + 0.3f * (income - 10.0f);
+        }
+
+        System.out.println("The total tax paid by the employee is: " + tax);
+--
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+
+        switch(day){
+            case 1 -> System.out.println("mon");
+            case 2 -> System.out.println("tue");
+            case 3 -> System.out.println("wed");
+            case 4 -> System.out.println("thr");
+            case 5 -> System.out.println("fri");
+            case 6 -> System.out.println("sat");
+            case 7 -> System.out.println("sun");
+        }
+--
+
+        Random r = new Random();
+        int a = r.nextInt();
+        System.out.println(a);
+--
+
+        Scanner sc = new Scanner(System.in);
+        String website = sc.next();
+        if(website.endsWith(".org")) {
+            System.out.println("It is org site..");
+        }
+        else if(website.endsWith(".com")){
+                System.out.println("It is com site..");
+        }
+        else if(website.endsWith(".in")){
+            System.out.println("It is indian site..");
+        }
+
+--
+ */
+
+
+    }
+}
