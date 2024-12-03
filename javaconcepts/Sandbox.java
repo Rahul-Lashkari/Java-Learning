01c54bed06f903faa779fa9c6369aafd54757973
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-03 22:48:58 +0530

    Updated Interfaces Revision with Q4
    Implemented multiple interfaces (Flyable and Swimmable) in Duck.

--- javaconcepts/Sandbox.java
+++ javaconcepts/Sandbox.java
@@ -357,6 +357,26 @@ class Dog implements Animal {
     }
 }
 
+interface Flyable {
+    void fly();
+}
+
+interface Swimmable {
+    void swim();
+}
+
+class Duck implements Flyable, Swimmable {
+    @Override
+    public void fly() {
+        System.out.println("Duck is flying...");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Duck is swimming...");
+    }
+}
+
 public class Sandbox {
 
     static void multiplication(int n){
@@ -808,11 +828,16 @@ public class Sandbox {
         myCar.start();
         myCar.stop();
 ------------------------------------------------------------------------------------------------------------
- */
         // Interfaces revision prob 3~~
         Dog myDog = new Dog();
         myDog.speak();
         myDog.sleep(); // Calls the default method
+------------------------------------------------------------------------------------------------------------
+ */
+        // Interfaces revision prob 4~~
+        Duck duck = new Duck();
+        duck.fly();
+        duck.swim();
 
     }
 }
