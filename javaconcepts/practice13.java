cc83c2e42cd89f0d1f5f4598d6297a8c43357bf4
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-25 11:03:37 +0530

    Practice Set 13 - Problem 3
    Demonstrate getPriority() and setPriority() methods in Java threads.

--- javaconcepts/practice13.java
+++ javaconcepts/practice13.java
@@ -3,18 +3,12 @@ import java.util.ArrayList;
 
 class Thread1 extends Thread{
     public void run(){
-        try{
-            Thread.sleep(200);
-        }
-        catch(InterruptedException e){
-            e.printStackTrace();
-        }
         System.out.println("Welcome");
     }
 }
-class Thread2 extends Thread{
-    public void run(){
-        System.out.println("Good Morning");
+class Thread2 extends Thread {
+    public void run() {
+        System.out.println("Good morning");
     }
 }
 
@@ -27,10 +21,21 @@ public class practice13 {
 //        t2.start();
 
         // Q2
+//        Thread1 t1= new Thread1();
+//        Thread2 t2= new Thread2();
+//        t1.start();
+//        t2.start();
+
+        // Q3
         Thread1 t1= new Thread1();
         Thread2 t2= new Thread2();
         t1.start();
         t2.start();
+        t1.setPriority(5);
+        t2.setPriority(1);
+
+        System.out.println(t1.getPriority());
+        System.out.println(t2.getPriority());
 
     }
 }
