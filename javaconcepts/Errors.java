ecad72f8f82ed77e1ffbcf5110abd70c0e9a5675
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-25 14:50:31 +0530

    Explored Errors in Java
    Explored the following types of errors :-
    Syntax errors, Logical errors, Runtime errors - also called Exceptions

---
+++ javaconcepts/Errors.java
@@ -0,0 +1,14 @@
+package javaconcepts;
+
+// Explored the following types of errors :-
+// 1) Syntax errors
+// 2) Logical errors
+// 3) Runtime errors, also called Exceptions
+
+public class Errors {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        System.out.println(a+b);
+    }
+}
