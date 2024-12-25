b778ee7a3a128b4716b021018bc16b47257a4bd9
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-25 11:00:08 +0530

    Practice Set 13 - Problem 2
    Q2 :- Add a step method in the welcome thread of question 1 to delay its execution for 200ms.

--- javaconcepts/practice13.java
+++ javaconcepts/practice13.java
@@ -3,25 +3,34 @@ import java.util.ArrayList;
 
 class Thread1 extends Thread{
     public void run(){
-        while(true){
-            System.out.println("Welcome");
+        try{
+            Thread.sleep(200);
         }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("Welcome");
     }
 }
-class Thread2 extends Thread {
-    public void run() {
-        while (true){
-            System.out.println("Good Morning");
-        }
+class Thread2 extends Thread{
+    public void run(){
+        System.out.println("Good Morning");
     }
 }
 
 public class practice13 {
     public static void main(String[] args) {
         // Q1
-        Thread1 t1 = new Thread1();
-        Thread2 t2 = new Thread2();
+//        Thread1 t1 = new Thread1();
+//        Thread2 t2 = new Thread2();
+//        t1.start();
+//        t2.start();
+
+        // Q2
+        Thread1 t1= new Thread1();
+        Thread2 t2= new Thread2();
         t1.start();
         t2.start();
+
     }
 }
