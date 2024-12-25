c4d60c0d631ea87e0eeb57c26438420bc7b8f283
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-25 11:08:20 +0530

    Practice Set 13 - Q4 + Q5
    Practice Set on Thread Completed.

--- javaconcepts/practice13.java
+++ javaconcepts/practice13.java
@@ -14,28 +14,34 @@ class Thread2 extends Thread {
 
 public class practice13 {
     public static void main(String[] args) {
-        // Q1
+          // Q1
 //        Thread1 t1 = new Thread1();
 //        Thread2 t2 = new Thread2();
 //        t1.start();
 //        t2.start();
 
-        // Q2
+          // Q2
 //        Thread1 t1= new Thread1();
 //        Thread2 t2= new Thread2();
 //        t1.start();
 //        t2.start();
 
-        // Q3
-        Thread1 t1= new Thread1();
-        Thread2 t2= new Thread2();
-        t1.start();
-        t2.start();
-        t1.setPriority(5);
-        t2.setPriority(1);
+          // Q3
+//        Thread1 t1= new Thread1();
+//        Thread2 t2= new Thread2();
+//        t1.start();
+//        t2.start();
+//        t1.setPriority(5);
+//        t2.setPriority(1);
+//
+//        System.out.println(t1.getPriority());
+//        System.out.println(t2.getPriority());
 
-        System.out.println(t1.getPriority());
-        System.out.println(t2.getPriority());
+          // Q4 - How do you get the state of a given thread in Java?
+          // Answer - getState() method is used to get the state of a given thread in Java.
+
+          // Q5 - How do you get the reference to the current thread in Java?
+          // Answer - currentThread() method is used to reference the current thread in Java.
 
     }
 }
