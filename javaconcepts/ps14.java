4827d767d84a71278c06ea5101009231965226a1
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-28 17:58:13 +0530

    Practice Set 14 - Problem 1
    Write a java program to demonstrate syntax, logical 2 runtime errors.

---
+++ javaconcepts/ps14.java
@@ -0,0 +1,11 @@
+package javaconcepts;
+
+public class ps14 {
+    public static void main(String[] args) {
+        // Problem 1
+        // Syntax Error - int a = 7
+        int age = 78;
+        int year_born = 2000-78; // Logical error
+        System.out.println(6/0);
+    }
+}
