591bbd07b9041277a69c072424f8cc3ede19db2a
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-28 18:22:36 +0530

    Practice Set 14 - Problem 4
    Q4) Modify program in Q3 to throw a custom Exception if max retries are reached.

--- javaconcepts/ps14.java
+++ javaconcepts/ps14.java
@@ -1,49 +1,84 @@
 package javaconcepts;
-
 import java.util.Scanner;
+
+class MaxRetriesExceededException extends Exception {
+    public MaxRetriesExceededException(String message) {
+        super(message);
+    }
+}
 
 public class ps14 {
     public static void main(String[] args) {
+//-----------------------------------------------------------------------------------------------------------------
         // Problem 1
         // Syntax Error - int a = 7
         int age = 78;
         int year_born = 2000-78; // Logical error
         // System.out.println(6/0);
-
+//-----------------------------------------------------------------------------------------------------------------
         // Problem 2
-        try{
-            int a = 666/0;
-        }
-        catch (IllegalArgumentException e){
-            System.out.println("HeHe");
-        }
-        catch (ArithmeticException e){
-            System.out.println("Haha");
-        }
-
+//        try{
+//            int a = 666/0;
+//        }
+//        catch (IllegalArgumentException e){
+//            System.out.println("HeHe");
+//        }
+//        catch (ArithmeticException e){
+//            System.out.println("Haha");
+//        }
+//-----------------------------------------------------------------------------------------------------------------
         // Problem 3
+//        boolean flag = true;
+//        int [] marks = new int[3];
+//        marks[0] = 7;
+//        marks[1] = 56;
+//        marks[2] = 6;
+//        Scanner Sc = new Scanner(System.in);
+//        int index;
+//        int i = 0;
+//        while(flag && i<5){
+//            try {
+//                System.out.println("Enter the value of index: ");
+//                index = Sc.nextInt();
+//                System.out.println("The value of marks[index] is " + marks[index]);
+//                break;
+//            }
+//            catch (Exception e) {
+//                System.out.println("Invalid Index");
+//                i++;
+//            }
+//        }
+//        if(i>=5){
+//            System.out.println("Error");
+//        }
+//-----------------------------------------------------------------------------------------------------------------
+        // Problem 4
         boolean flag = true;
-        int [] marks = new int[3];
+        int[] marks = new int[3];
         marks[0] = 7;
         marks[1] = 56;
         marks[2] = 6;
         Scanner Sc = new Scanner(System.in);
         int index;
         int i = 0;
-        while(flag && i<5){
+
+        while (flag) {
             try {
+                if (i >= 5) {
+                    throw new MaxRetriesExceededException("Maximum retries exceeded! Please try again later.");
+                }
+
                 System.out.println("Enter the value of index: ");
                 index = Sc.nextInt();
                 System.out.println("The value of marks[index] is " + marks[index]);
                 break;
-            }
-            catch (Exception e) {
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Invalid Index");
                 i++;
+            } catch (MaxRetriesExceededException e) {
+                System.out.println(e.getMessage());
+                break;
             }
-        }
-        if(i>=5){
-            System.out.println("Error");
         }
     }
 }
