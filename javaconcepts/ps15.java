19067ef03de44deae45159099bf3db2f000e5a92
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-01-02 23:21:43 +0530

    Practice Set 15 - Problem 3
    Repeat question number 2 using the Calendar class.

--- javaconcepts/ps15.java
+++ javaconcepts/ps15.java
@@ -26,6 +26,11 @@ public class ps15 {
         Date d = new Date();
         System.out.println(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
 
+        // PS Q3 - Repeat question number 2 using the Calendar class.
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND));
+
+
         // PS Q5 - Create a Set in java. Try to store the duplicate values elements inside this set and verify that only one instance is stored.
         HashSet<Integer> s = new HashSet();
         s.add(5);
